99ec5c94f745a719d8ce2880dbace36d62ae964c
Jacek Pietal <prozi85@gmail.com>
2020-06-26 20:34:16 +0200

    fix(tdd): fix thanks to tests

--- dist/uws.js
+++ dist/uws.js
@@ -22,52 +22,52 @@ let uWebSockets;
 try {
   console.log(`Loading µWS version: ${version}`);
   /**
-   * @typedef {uWebSockets.js:SSLApp|uWebSockets.js:App} TemplatedApp
-   * TemplatedApp is either an SSL or non-SSL app. See App for more info, read user manual. 
-
+   * @typedef {uws.js:SSLApp|uws.js:App} TemplatedApp
+   * TemplatedApp is either an SSL or non-SSL app. See App for more info, read user manual.
+   *
    * @property {function} listen (host: RecognizedString, port: number, cb: (listenSocket: us_listen_socket) => void): TemplatedApp;
    * Listens to hostname & port. Callback hands either false or a listen socket.
-
+   *
    * @property {function} listen (port: number, cb: (listenSocket: any) => void): TemplatedApp;
-   * Listens to port. Callback hands either false or a listen socket. 
+   * Listens to port. Callback hands either false or a listen socket.
    *
    * @property {function} get (pattern: RecognizedString, handler: (res: HttpResponse, req: HttpRequest) => void) : TemplatedApp;
-   * Registers an HTTP GET handler matching specified URL pattern. 
+   * Registers an HTTP GET handler matching specified URL pattern.
    *
    * @property {function} post (pattern: RecognizedString, handler: (res: HttpResponse, req: HttpRequest) => void) : TemplatedApp;
-   * Registers an HTTP POST handler matching specified URL pattern. 
+   * Registers an HTTP POST handler matching specified URL pattern.
    *
    * @property {function} options (pattern: RecognizedString, handler: (res: HttpResponse, req: HttpRequest) => void) : TemplatedApp;
-   * Registers an HTTP OPTIONS handler matching specified URL pattern. 
+   * Registers an HTTP OPTIONS handler matching specified URL pattern.
    *
    * @property {function} del (pattern: RecognizedString, handler: (res: HttpResponse, req: HttpRequest) => void) : TemplatedApp;
-   * Registers an HTTP DELETE handler matching specified URL pattern. 
+   * Registers an HTTP DELETE handler matching specified URL pattern.
    *
    * @property {function} patch (pattern: RecognizedString, handler: (res: HttpResponse, req: HttpRequest) => void) : TemplatedApp;
-   * Registers an HTTP PATCH handler matching specified URL pattern. 
+   * Registers an HTTP PATCH handler matching specified URL pattern.
    *
    * @property {function} put (pattern: RecognizedString, handler: (res: HttpResponse, req: HttpRequest) => void) : TemplatedApp;
-   * Registers an HTTP PUT handler matching specified URL pattern. 
+   * Registers an HTTP PUT handler matching specified URL pattern.
    *
    * @property {function} head (pattern: RecognizedString, handler: (res: HttpResponse, req: HttpRequest) => void) : TemplatedApp;
-   * Registers an HTTP HEAD handler matching specified URL pattern. 
+   * Registers an HTTP HEAD handler matching specified URL pattern.
    *
    * @property {function} connect (pattern: RecognizedString, handler: (res: HttpResponse, req: HttpRequest) => void) : TemplatedApp;
-   * Registers an HTTP CONNECT handler matching specified URL pattern. 
+   * Registers an HTTP CONNECT handler matching specified URL pattern.
    *
    * @property {function} trace (pattern: RecognizedString, handler: (res: HttpResponse, req: HttpRequest) => void) : TemplatedApp;
-   * Registers an HTTP TRACE handler matching specified URL pattern. 
+   * Registers an HTTP TRACE handler matching specified URL pattern.
    *
    * @property {function} any (pattern: RecognizedString, handler: (res: HttpResponse, req: HttpRequest) => void) : TemplatedApp;
-   * Registers an HTTP handler matching specified URL pattern on any HTTP method. 
+   * Registers an HTTP handler matching specified URL pattern on any HTTP method.
    *
    * @property {function} ws (pattern: RecognizedString, behavior: WebSocketBehavior) : TemplatedApp;
-   * Registers a handler matching specified URL pattern where WebSocket upgrade requests are caught. 
+   * Registers a handler matching specified URL pattern where WebSocket upgrade requests are caught.
    *
    * @property {function} publish (topic: RecognizedString, message: RecognizedString, isBinary?: boolean, compress?: boolean) : TemplatedApp;
-   * Publishes a message under topic, for all WebSockets under this app. See WebSocket.publish. 
+   * Publishes a message under topic, for all WebSockets under this app. See WebSocket.publish.
    */
-  uWebSockets = require(`uWebSockets.js/uws_${version}.node`);
+  uWebSockets = require(`uws.js/uws_${version}.node`);
 
   if (process.env.EXPERIMENTAL_FASTCALL) {
     process.nextTick = (f, ...args) => {
